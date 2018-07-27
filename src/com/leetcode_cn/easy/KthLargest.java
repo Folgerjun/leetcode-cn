47158cbe21ce6d8619bb1d62c4474f641e13c7bd
ffj <ffj0721@163.com>
2018-07-27 11:58:18 +0800

    数据流中的第K大元素

---
+++ src/com/leetcode_cn/easy/KthLargest.java
@@ -0,0 +1,67 @@
+package com.leetcode_cn.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*************数据流中的第K大元素************************/
+/**
+ * 设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
+ * 
+ * 你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用
+ * KthLargest.add，返回当前数据流中第K大的元素。
+ * 
+ * 示例:
+ * 
+ * int k = 3; int[] arr = [4,5,8,2];
+ * 
+ * KthLargest kthLargest = new KthLargest(3, arr);
+ * 
+ * kthLargest.add(3); // returns 4
+ * 
+ * kthLargest.add(5); // returns 5
+ * 
+ * kthLargest.add(10); // returns 5
+ * 
+ * kthLargest.add(9); // returns 8
+ * 
+ * kthLargest.add(4); // returns 8
+ * 
+ * 说明: 你可以假设 nums 的长度≥ k-1 且k ≥ 1。
+ * 
+ * @author ffj
+ *
+ */
+public class KthLargest {
+
+	public static void main(String[] args) {
+		int k = 3;
+		int[] arr = { 4, 5, 8, 2 };
+		KthLargest kthLargest = new KthLargest(k, arr);
+		int result = 0;
+		result = kthLargest.add(3); // returns 4
+		result = kthLargest.add(5); // returns 5
+		result = kthLargest.add(10); // returns 5
+		result = kthLargest.add(9); // returns 8
+		result = kthLargest.add(4); // returns 8
+		System.out.println(result);
+	}
+
+	private List<Integer> numList = new ArrayList<>();
+
+	private int num;
+
+	public KthLargest(int k, int[] nums) {
+		for (int n : nums) {
+			numList.add(n);
+		}
+		num = k;
+	}
+
+	public int add(int val) {
+		numList.add(val);
+		Collections.sort(numList);
+		return numList.get(numList.size() - num);
+	}
+
+}
