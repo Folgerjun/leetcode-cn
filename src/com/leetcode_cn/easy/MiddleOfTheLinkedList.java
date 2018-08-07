fe99d3b9cf3ea7c15b60316abe27679b7613b363
ffj <ffj0721@163.com>
2018-08-07 16:20:26 +0800

    链表的中间结点

---
+++ src/com/leetcode_cn/easy/MiddleOfTheLinkedList.java
@@ -0,0 +1,49 @@
+package com.leetcode_cn.easy;
+
+/********************链表的中间结点*******************/
+/**
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ * 
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * 
+ * 示例 1：
+ * 
+ * 输入：[1,2,3,4,5]
+ * 
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5]) 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样： ans.val = 3, ans.next.val = 4,
+ * ans.next.next.val = 5, 以及 ans.next.next.next = NULL. 示例 2：
+ * 
+ * 输入：[1,2,3,4,5,6]
+ * 
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6]) 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ * 
+ * 提示：
+ * 
+ * 给定链表的结点数介于 1 和 100 之间。
+ * 
+ * @author ffj
+ *
+ */
+public class MiddleOfTheLinkedList {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode middleNode(ListNode head) {
+		ListNode node = head, middleNode = head;
+		while (node != null && node.next != null) { // 速度相差两倍 快的一方到链尾 慢的一方就是链中间
+			middleNode = middleNode.next;
+			node = node.next.next;
+		}
+		return middleNode;
+	}
+
+}
